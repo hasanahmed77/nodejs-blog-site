c8faed3f1c41daeb64a4b81232d82754f9e4a757
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2024-04-04 21:39:10 +0600

    changed swagger api

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use("/create", blogRoutes)
 
 // Swagger docs
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Test
 // app.get("/", (req, res) => res.send("Express on Vercel"));
