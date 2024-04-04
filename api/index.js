c17b714fcb2ed1571098b937a48c4e40089a88f9
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2024-04-05 03:53:25 +0600

    changed swagger css

--- api/index.js
+++ api/index.js
@@ -8,9 +8,15 @@ const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
+const options = {
+  customCssUrl: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+  ]
+};
+
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+// swaggerUi.setup(specs, { customCssUrl: CSS_URL })
 
 // Loading Swagger YAML file
 const swaggerDocument = YAML.load(path.join(__dirname, '../blog.yaml'));
@@ -25,7 +31,7 @@ app.use(express.json())
 app.use("/create", blogRoutes)
 
 // Swagger docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 // Test
 // app.get("/", (req, res) => res.send("Express on Vercel"));
